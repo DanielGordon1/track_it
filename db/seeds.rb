7263d6fdc7f5d4806a4ec5f57f3b9eb04c30e7a6
eden laboda <eden.laboda@gmail.com>
2016-03-15 15:56:13 +0100

    make index track into a partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Track.create(name: "awesome track", genre: "hip-hop", version: "final", user_id: 1)
+
+

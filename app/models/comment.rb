b77c6f69606e92b642a51a4e4b5fcaa512ec0b96
eden laboda <eden.laboda@gmail.com>
2016-03-15 11:43:06 +0100

    routes and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :track
   belongs_to :user
+
+  validates :content, :track_id, :user_id, presence: :true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, :genre, :version, :user_id, presence: :true
+
 end

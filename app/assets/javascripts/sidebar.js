6921285483e1d416f8ac2884c9c08ed5a735b2c0
Daniel Gordon <d_gordon1@icloud.com>
2016-03-15 16:45:14 +0100

    Fix javascript files

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -2,15 +2,12 @@ var sidebarBox = document.querySelector('#box'),
 sidebarBtn = document.querySelector('#btn'),
 pageWrapper = document.querySelector('#page-wrapper');
 
-sidebarBtn.addEventListener('click', event => {
+sidebarBtn.addEventListener('click', function(event) {
   sidebarBtn.classList.toggle('active');
   sidebarBox.classList.toggle('active');
 });
 
-
-
-window.addEventListener('keydown', event => {
-
+window.addEventListener('keydown', function(event) {
   if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
     sidebarBtn.classList.remove('active');
     sidebarBox.classList.remove('active');

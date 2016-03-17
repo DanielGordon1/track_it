3f3752ddbdaa0617ec9ebaecf2ab461a1966a1fc
WouterKompier <wouterjckompier@gmail.com>
2016-03-17 17:05:42 +0100

    add upvote/downvote for tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,33 +1,47 @@
 class TracksController < ApplicationController
 
-before_action :find_track, only: [:show, :upvote, :destroy]
+  before_action :find_track, only: [:show, :upvote, :downvote, :destroy]
 
 
-def show
-  @comment = Comment.new
-end
-
-def new
-  @track = current_user.tracks.new
-end
-
-def create
-  @track = current_user.tracks.new(track_params)
-  if @track.save
-  redirect_to track_path(@track)
-  else
-    render :new
+  def show
+    @comment = Comment.new
   end
-end
 
-def destroy
-  @cocktail.destroy
-  redirect_to current_user
-end
+  def new
+    @track = current_user.tracks.new
+  end
 
-def upvote
-  @track.votes.where(user: current_user).first_or_create
-  redirect_to root_path(anchor: "hot-tracks")
+  def create
+    @track = current_user.tracks.new(track_params)
+    if @track.save
+      redirect_to track_path(@track)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @track.destroy
+    redirect_to current_user
+  end
+
+  def upvote
+    if current_user.votes.where(user: current_user).exists?
+      flash[:notice] = "You have already upvoted this, Don't be Greeeeeddyyyyy!!!"
+      redirect_to root_path(anchor: "hot-tracks")
+    else 
+     @track.votes.where(user: current_user).first_or_create
+     redirect_to root_path(anchor: "hot-tracks")
+   end
+ end
+
+ def downvote
+  if current_user.votes.where(user: current_user).exists?
+    flash[:notice] = "You Really Want To Downvote this Balss MEga TRAck?"
+    @track.votes.where(user: current_user).first.destroy
+    redirect_to root_path(anchor: "hot-tracks")
+ end
+
 end
 
 private

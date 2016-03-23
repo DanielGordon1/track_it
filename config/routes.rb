11fb3466f15c63d1403b9f9e1d923cd178a11a95
Daniel Gordon <d_gordon1@icloud.com>
2016-03-23 16:52:19 +0100

    fixed routes and updated profile show

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # Souncloud Omniauth
-  get '/soundcloud/auth', to: 'soundcloud#auth'
-  get '/soundcloud/callback', to: 'soundcloud#callback'
 
   devise_for :users
 
+  get '/soundcloud/auth', to: 'soundcloud#auth'
+  get '/soundcloud/callback', to: 'soundcloud#callback'
+  
   resources :trackers, only: [:show]
 
   resources :tracks, only: [:show, :new, :create, :destroy] do

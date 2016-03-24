f578b92f48bc597616fa67260796d590db667393
eden laboda <eden.laboda@gmail.com>
2016-03-24 14:28:53 +0100

    ajaxify the upvotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
    skip_before_action :authenticate_user!
 
   def home
-    @tracks = Track.all
+    @tracks = Track.where.not(soundcloud_permalink_url: nil)
+     # the track permalink can't be nil
   end
 end

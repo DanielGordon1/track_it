994382aa34abf03fd9aa0448579f9204027e3b26
lennard ypma <ypmalennard@hotmail.com>
2016-03-25 14:27:19 +0100

    gets index page to load tracks when scrolled down

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
    skip_before_action :authenticate_user!
 
   def home
-    @tracks = Track.where.not(soundcloud_permalink_url: nil)
+    tracks = Track.where.not(soundcloud_permalink_url: nil)
+    @tracks = Kaminari.paginate_array(tracks).page(params[:page]).per(3)
      # the track permalink can't be nil
   end
 end

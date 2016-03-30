3d4ecf3c8e14cf575bbf77b116f90486a26563d1
Daniel Gordon <d_gordon1@icloud.com>
2016-03-30 16:37:51 +0200

    Updates the trackershow with the sqlmethods in the controller

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -3,9 +3,9 @@ class TrackersController < ApplicationController
 
   def show
     @tracker = User.find(params[:id])
+    @tracks_tracker = @tracker.tracks.order(created_at: :desc)
+    @upvoted_tracks_tracker = Track.joins(:votes).where(votes: { user_id: @tracker.id }).select("tracks.*, MAX(votes.created_at) as vote_created_at").order("vote_created_at DESC").group("tracks.id")
+    @commented_tracks_tracker = Track.joins(:comments).where(comments: { user_id: @tracker.id }).select("tracks.*, MAX(comments.created_at) as comment_created_at").order("comment_created_at DESC").group("tracks.id")
   end
 
 end
-
-
-
